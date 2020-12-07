9956cd61ab2ff2da8c6d16f70fda1de94f374d01
Wenley Tong <wenley.tong@gmail.com>
2020-12-06 21:47:20 -0800

    fixup! Add Day 2 solution

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -4,22 +4,24 @@ use std::io;
 use std::fs;
 use std::io::BufRead;
 use std::path::Path;
-use std::collections::HashSet;
 use regex::Regex;
 
 fn main() {
-    let io_result = lines_in_file("day1input.txt");
-    let mut numbers = HashSet::new();
+    let io_result = lines_in_file("day2input.txt");
     match io_result {
         Ok(lines) => {
+            let mut valid_rows = 0;
             for line in lines {
                 match line {
                     Ok(stuff) => {
-
+                        if parse_line(&stuff).valid() {
+                            valid_rows += 1;
+                        }
                     }
-                    Err(e) => panic!("Error reading line"),
+                    Err(_) => panic!("Error reading line"),
                 }
             }
+            println!("{} rows passed rules", valid_rows);
         },
         Err(_) => panic!("Error reading file"),
     }
@@ -30,13 +32,32 @@ fn lines_in_file<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<fs::File
     Ok(io::BufReader::new(file).lines())
 }
 
-struct PasswordRule {
+struct PasswordRule<'a> {
     min_count: i64,
     max_count: i64,
     letter: char,
-    test_password: &str,
+    test_password: &'a str,
 }
 
 fn parse_line(line: &str) -> PasswordRule {
-    const parse_regex = None;
+    let parse_regex: Regex = Regex::new(r"^(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]): (?P<pass>[a-z]+)$").unwrap();
+    let captures: regex::Captures = match parse_regex.captures(line) {
+        Some(thing) => thing,
+        None => panic!("Couldn't parse {}", line),
+    };
+
+    PasswordRule {
+        min_count: captures.name("min").unwrap().as_str().parse().unwrap(),
+        max_count: captures.name("max").unwrap().as_str().parse().unwrap(),
+        letter: captures.name("letter").unwrap().as_str().chars().next().unwrap(),
+        test_password: captures.name("pass").unwrap().as_str(),
+    }
+}
+
+impl PasswordRule<'_> {
+    fn valid(&self) -> bool {
+        let letter_counts = self.test_password.chars().filter(|c| *c == self.letter).count() as i64;
+
+        letter_counts >= self.min_count && letter_counts <= self.max_count
+    }
 }
