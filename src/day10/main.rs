3f28a18b2af7ce909615328b371c512df2e0cecc
Wenley Tong <wenley.tong@gmail.com>
2020-12-13 22:59:49 -0800

    Day 10 Parts 1 and 2

--- src/day10/main.rs
+++ src/day10/main.rs
@@ -4,14 +4,49 @@ use std::io;
 use std::fs;
 use std::io::BufRead;
 use std::path::Path;
-use std::fmt;
+use std::collections::HashMap;
 
 fn main() {
     let input = parse_input();
+    let mut nums = input.rows;
+    nums.sort();
+    println!("{:?}", nums.iter().take(10).map(|i| *i).collect::<Vec<i64>>());
+
+    let mut one_gaps = 0;
+    let mut three_gaps = 1; // Device
+    let mut old_value = 0;
+    nums.iter().for_each(|new_value| {
+        let diff = new_value - old_value;
+        match diff {
+            3 => { three_gaps += 1; }
+            2 => {},
+            1 => { one_gaps += 1; }
+            _ => panic!("Unknown gap! {} = {} - {}", diff, new_value, old_value)
+        };
+        old_value = *new_value;
+    });
+    println!("{} * {} = {}", one_gaps, three_gaps, one_gaps * three_gaps);
+
+    let max_value = nums.iter().max().unwrap();
+    let mapping = ways_to_reach(&nums);
+    println!("{} ways to get to adapter", mapping.get(&max_value).unwrap());
+}
+
+fn ways_to_reach(sorted_nums: &Vec<i64>) -> HashMap<i64, usize> {
+    let mut cache = HashMap::new();
+    cache.insert(0, 1);
+    for n in sorted_nums {
+        let mut ways = 0;
+        for m in n-3..*n {
+            ways += cache.get(&m).unwrap_or(&0);
+        }
+        cache.insert(*n, ways);
+    }
+    cache
 }
 
 struct InputData {
-    rows: Vec<usize>,
+    rows: Vec<i64>,
 }
 
 impl InputData {
