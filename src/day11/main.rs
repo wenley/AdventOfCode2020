838e537ef0f7a6a5b21cede51edf3f8b3b47e2c6
Wenley Tong <wenley.tong@gmail.com>
2020-12-13 23:28:40 -0800

    Implement Part 1, Day 11

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -7,16 +7,83 @@ use std::path::Path;
 
 fn main() {
     let input = parse_input();
+    let mut ferry = input.ferry;
+    let mut next_ferry = ferry.next_ferry();
+
+    while ferry != next_ferry {
+        ferry = next_ferry;
+        next_ferry = ferry.next_ferry();
+    }
+    println!("Finished!");
+    let occupied_count: usize = ferry.spots.iter().map(|row| {
+        row.iter().filter(|spot| **spot == Spot::Occupied).count()
+    }).sum();
+    println!(
+        "{}",
+        ferry.spots.iter().map(|row| {
+            row.iter().map(|spot| match spot {
+                Spot::Floor => ".",
+                Spot::Empty => "L",
+                Spot::Occupied => "#",
+            }).collect::<Vec<&str>>().join("")
+        }).collect::<Vec<String>>().join("\n"),
+    );
+    println!("{} occupied seats", occupied_count);
 }
 
+#[derive(PartialEq, Clone, Copy)]
 enum Spot {
     Floor,
     Empty,
     Occupied,
 }
 
+#[derive(PartialEq)]
 struct Ferry {
     spots: Vec<Vec<Spot>>,
+}
+
+const NEIGHBOR_DELTAS: [(i64, i64); 8] = [
+    (-1, -1), (-1, 0), (-1, 1),
+    ( 0, -1),          ( 0, 1),
+    ( 1, -1), ( 1, 0), ( 1, 1),
+];
+
+impl Ferry {
+    fn next_ferry(&self) -> Ferry {
+        Ferry {
+            spots: self.spots.iter().enumerate().map(|(row, row_spots)| {
+                row_spots.iter().enumerate().map(|(col, current)| {
+                    match (current, self.occupied_neighbors(row as i64, col as i64)) {
+                        (Spot::Floor, _) => Spot::Floor,
+                        (_, 0) => Spot::Occupied,
+                        (_, 4..=8) => Spot::Empty,
+                        _ => *current,
+                    }
+                }).collect()
+            }).collect()
+        }
+    }
+
+    fn occupied_neighbors(&self, row: i64, col: i64) -> usize {
+        NEIGHBOR_DELTAS.iter().map(|(dx, dy)| {
+            match self.get(row + dx, col + dy) {
+                Spot::Occupied => 1,
+                _ => 0,
+            }
+        }).sum()
+    }
+
+    fn get(&self, row: i64, col: i64) -> Spot {
+        if row < 0 || row >= self.spots.len() as i64 {
+            return Spot::Empty;
+        }
+        let spot_row = &self.spots[row as usize];
+        if col < 0 || col >= spot_row.len() as i64 {
+            return Spot::Empty;
+        }
+        spot_row[col as usize]
+    }
 }
 
 struct InputData {
