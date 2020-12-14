10c81009a8fc6e608fc609667587e25088d54eb8
Wenley Tong <wenley.tong@gmail.com>
2020-12-13 22:41:47 -0800

    Add skeleton for Day 10

---
+++ src/day10/main.rs
@@ -0,0 +1,42 @@
+extern crate regex;
+
+use std::io;
+use std::fs;
+use std::io::BufRead;
+use std::path::Path;
+use std::fmt;
+
+fn main() {
+    let input = parse_input();
+}
+
+struct InputData {
+    rows: Vec<usize>,
+}
+
+impl InputData {
+}
+
+fn parse_input() -> InputData {
+    let io_result = lines_in_file("day10input.txt");
+    match io_result {
+        Ok(lines) => {
+            let rows = lines.map(|line| match line {
+                Ok(stuff) => {
+                    stuff.parse().unwrap()
+                }
+                Err(_) => panic!("Error reading line"),
+            }).collect();
+            InputData {
+                rows: rows,
+            }
+        },
+        Err(_) => panic!("Error reading file"),
+    }
+}
+
+fn lines_in_file<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<fs::File>>> where P: AsRef<Path> {
+    let file = fs::File::open(file_path)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
