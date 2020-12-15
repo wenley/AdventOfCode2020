61c6ba84c80c24b6f88699cf9ff137317d926142
Wenley Tong <wenley.tong@gmail.com>
2020-12-14 19:05:04 -0800

    Partial Part 2 solution

--- src/day13b.rs
+++ src/day13b.rs
@@ -8,38 +8,86 @@ use std::path::Path;
 fn main() {
     let input = parse_input();
     let buses = input.buses;
-    let other_buses = &buses[1..];
-    let product = buses.iter().map(|b| match b {
-        Constraint::Bus(x) => *x,
-        Constraint::Blank => 1,
-    }).fold(1, |acc, num| acc * num);
-    println!("product = {}", product);
-    match buses[0] {
-        Constraint::Bus(x) => {
-            for i in (0..product).step_by(x) {
-                let times = i..(i + other_buses.len());
-                if times.zip(other_buses.iter()).all(|(t, bus)| bus.matches(t)) {
-                    println!("Time t = {} works!", i);
-                    break;
+    let rules = buses.
+        iter().
+        filter(|b| **b == Constraint::Blank).
+        enumerate().
+        map(|(i, b)| {
+            match b {
+                Constraint::Bus(num) => {
+                    let p = *num as i64;
+                    Rule {
+                        remainder: -(i as i64) % p,
+                        modulo: p,
+                    }
                 }
+                Constraint::Blank => panic!("Filter didn't work"),
             }
-        },
-        Constraint::Blank => panic!("First bus is not a bus"),
+        });
+
+    println!("{:?}", euclid(27, 20));
+}
+
+/*
+ * function extended_gcd(a, b)
+    (old_r, r) := (a, b)
+    (old_s, s) := (1, 0)
+    (old_t, t) := (0, 1)
+
+    while r ≠ 0 do
+        quotient := old_r div r
+        (old_r, r) := (r, old_r − quotient × r)
+        (old_s, s) := (s, old_s − quotient × s)
+        (old_t, t) := (t, old_t − quotient × t)
+
+    output "Bézout coefficients:", (old_s, old_t)
+    output "greatest common divisor:", old_r
+    output "quotients by the gcd:", (t, s)
+ */
+fn euclid(n1: i64, n2: i64) -> (i64, i64) {
+    if n1 < n2 { panic!("n1 must be at least n2"); }
+
+    let (mut old_r, mut r) = (n1, n2);
+    let (mut old_s, mut s) = (1, 0);
+    let (mut old_t, mut t) = (0, 1);
+
+    while r != 0 {
+        let quotient = old_r / r;
+        let (new_r, new_s, new_t) = (
+            old_r - quotient * r,
+            old_s - quotient * s,
+            old_t - quotient * t,
+        );
+        old_r = r;
+        r = new_r;
+        old_s = s;
+        s = new_s;
+        old_t = t;
+        t = new_t;
+    }
+
+    (old_s, old_t)
+}
+
+struct Rule {
+    remainder: i64,
+    modulo: i64,
+}
+
+impl Rule {
+    fn merge(&self, other: &Rule) -> Rule {
+        if self.modulo >= other.modulo {
+            let (m1, m2) = euclid(self.modulo, other.modulo);
+        } else {
+            let (m2, m1) = euclid(self.modulo, other.modulo);
+        }
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
 enum Constraint {
     Bus(usize),
     Blank,
-}
-
-impl Constraint {
-    fn matches(&self, timestamp: usize) -> bool {
-        match self {
-            Constraint::Blank => true,
-            Constraint::Bus(i) => timestamp % i == 0,
-        }
-    }
 }
 
 struct InputData {
