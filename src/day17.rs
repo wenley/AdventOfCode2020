75f81d4ac3be8e8fe49777ec84c02a1dd12975c2
Wenley Tong <wenley.tong@gmail.com>
2020-12-16 21:43:43 -0800

    Day 17 Part 2

--- src/day17.rs
+++ src/day17.rs
@@ -26,48 +26,40 @@ enum Cell {
 struct Space {
     cells: HashMap<Point, Cell>,
 }
-type Point = (i64, i64, i64);
-
-const NEIGHBOR_DELTAS: [Point; 26] = [
-    (-1, -1, -1), (-1, 0, -1), (-1, 1, -1),
-    ( 0, -1, -1), ( 0, 0, -1), ( 0, 1, -1),
-    ( 1, -1, -1), ( 1, 0, -1), ( 1, 1, -1),
-
-    (-1, -1,  0), (-1, 0,  0), (-1, 1,  0),
-    ( 0, -1,  0),              ( 0, 1,  0),
-    ( 1, -1,  0), ( 1, 0,  0), ( 1, 1,  0),
-
-    (-1, -1,  1), (-1, 0,  1), (-1, 1,  1),
-    ( 0, -1,  1), ( 0, 0,  1), ( 0, 1,  1),
-    ( 1, -1,  1), ( 1, 0,  1), ( 1, 1,  1),
-];
+type Point = (i64, i64, i64, i64);
 
 fn neighbor_deltas() -> Vec<Point> {
     (-1..=1).flat_map(|x| {
         (-1..=1).flat_map(|y| {
-            (-1..=1).map(|z| {
-                if x == 0 && y == 0 && z == 0 {
-                    None
-                } else {
-                    Some((x, y, z))
-                }
-            }).filter_map(|maybe| maybe).collect::<Vec<_>>()
+            (-1..=1).flat_map(|z| {
+                (-1..=1).map(|w| {
+                    if x == 0 && y == 0 && z == 0 && w == 0 {
+                        None
+                    } else {
+                        Some((x, y, z, w))
+                    }
+                }).filter_map(|maybe| maybe).collect::<Vec<_>>()
+            }).collect::<Vec<_>>()
         }).collect::<Vec<_>>()
     }).collect::<Vec<_>>()
 }
 
 impl Space {
     fn next_space(&self) -> Space {
-        let min_x = self.cells.keys().map(|(x, _, _)| x).min().unwrap() - 1;
-        let min_y = self.cells.keys().map(|(_, y, _)| y).min().unwrap() - 1;
-        let min_z = self.cells.keys().map(|(_, _, z)| z).min().unwrap() - 1;
-        let max_x = self.cells.keys().map(|(x, _, _)| x).max().unwrap() + 1;
-        let max_y = self.cells.keys().map(|(_, y, _)| y).max().unwrap() + 1;
-        let max_z = self.cells.keys().map(|(_, _, z)| z).max().unwrap() + 1;
+        let min_x = self.cells.keys().map(|(x, _, _, _)| x).min().unwrap() - 1;
+        let min_y = self.cells.keys().map(|(_, y, _, _)| y).min().unwrap() - 1;
+        let min_z = self.cells.keys().map(|(_, _, z, _)| z).min().unwrap() - 1;
+        let min_w = self.cells.keys().map(|(_, _, _, w)| w).min().unwrap() - 1;
+        let max_x = self.cells.keys().map(|(x, _, _, _)| x).max().unwrap() + 1;
+        let max_y = self.cells.keys().map(|(_, y, _, _)| y).max().unwrap() + 1;
+        let max_z = self.cells.keys().map(|(_, _, z, _)| z).max().unwrap() + 1;
+        let max_w = self.cells.keys().map(|(_, _, _, w)| w).max().unwrap() + 1;
 
         let points: Vec<Point> = (min_x..=max_x).flat_map(|x| {
             (min_y..=max_y).flat_map(|y| {
-                (min_z..=max_z).map(|z| (x, y, z)).collect::<Vec<Point>>()
+                (min_w..=max_w).flat_map(|w| {
+                    (min_z..=max_z).map(|z| (x, y, z, w)).collect::<Vec<Point>>()
+                }).collect::<Vec<Point>>()
             }).collect::<Vec<Point>>()
         }).collect();
 
@@ -83,9 +75,9 @@ impl Space {
     }
 
     fn occupied_neighbors(&self, point: &Point) -> usize {
-        let (x, y, z) = point;
-        NEIGHBOR_DELTAS.iter().map(|(dx, dy, dz)| {
-            let new_point = (x + dx, y + dy, z + dz);
+        let (x, y, z, w) = point;
+        neighbor_deltas().iter().map(|(dx, dy, dz, dw)| {
+            let new_point = (x + dx, y + dy, z + dz, w + dw);
             match self.cells.get(&new_point) {
                 Some(Cell::Alive) => 1,
                 _ => 0,
@@ -108,7 +100,7 @@ fn parse_input() -> InputData {
                         chars().
                         enumerate().
                         filter(|(_, c)| *c == '#').
-                        map(|(j, _)| ((i as i64, j as i64, 0), Cell::Alive)).
+                        map(|(j, _)| ((i as i64, j as i64, 0, 0), Cell::Alive)).
                         collect::<Vec<_>>()
                 }
                 Err(_) => panic!("Error reading line"),
