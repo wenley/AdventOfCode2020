c029a18a1bc5fccd3e15ff374b0f4276ef0d0fef
Wenley Tong <wenley.tong@gmail.com>
2020-12-19 21:20:13 -0800

    Setup infra to find corners

--- src/day20.rs
+++ src/day20.rs
@@ -3,21 +3,79 @@ extern crate regex;
 use std::io;
 use std::fs;
 use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 fn main() {
     let input = parse_input().unwrap();
 }
 
+#[derive(Hash, Debug, Clone, Copy)]
 enum Pixel {
     On,
     Off,
 }
+#[derive(Hash, Debug, Clone)]
 struct Tile {
     pixels: Vec<Vec<Pixel>>,
 }
 
+#[derive(Hash, Debug, Clone)]
+struct Edge {
+    pixels: Vec<Pixel>
+}
+
+impl Tile {
+    fn edges(&self) -> Vec<Edge> {
+        let mut edges = vec![];
+        edges.push(Edge {
+            pixels: self.pixels.
+                first().
+                map(|vec| vec.iter().map(|p| *p).collect()).
+                unwrap(),
+        });
+        edges.push(Edge {
+            pixels: self.pixels.
+                last().
+                map(|vec| vec.iter().map(|p| *p).collect()).
+                unwrap(),
+        });
+        edges.push(Edge {
+            pixels: self.pixels.
+                iter().
+                map(|vec| vec.first().map(|p| *p).unwrap()).
+                collect(),
+        });
+        edges.push(Edge {
+            pixels: self.pixels.
+                iter().
+                map(|vec| vec.last().map(|p| *p).unwrap()).
+                collect(),
+        });
+
+        edges
+    }
+}
+
 struct InputData {
     tiles: Vec<Tile>,
+}
+impl Edge {
+    fn identifier(&self) -> u64 {
+        let mut forward_hasher = DefaultHasher::new();
+        self.pixels.hash(&mut forward_hasher);
+        let forward = forward_hasher.finish();
+
+        let mut backward_hasher = DefaultHasher::new();
+        self.pixels.iter().rev().collect::<Vec<_>>().hash(&mut backward_hasher);
+        let backward = backward_hasher.finish();
+
+        if forward <= backward {
+            forward
+        } else {
+            backward
+        }
+    }
 }
 
 fn parse_input() -> io::Result<InputData> {
