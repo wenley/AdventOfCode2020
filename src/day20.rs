da81a0cf9f11650026d7df2cb24884b4e76a418b
Wenley Tong <wenley.tong@gmail.com>
2020-12-19 21:36:34 -0800

    Add identifier parsing for Tiles

--- src/day20.rs
+++ src/day20.rs
@@ -5,33 +5,37 @@ use std::fs;
 use std::collections::HashMap;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use regex::Regex;
 
 fn main() {
     let input = parse_input().unwrap();
 
-    let mut edge_to_tile_count: HashMap<Edge, usize>  = HashMap::new();
+    let mut edge_to_tile_count: HashMap<u64, usize>  = HashMap::new();
     input.tiles.iter().for_each(|tile| {
         tile.edges().iter().for_each(|edge| {
-            match edge_to_tile_count.get(edge) {
+            let id = edge.identifier();
+            match edge_to_tile_count.remove(&id) {
                 None => {
-                    edge_to_tile_count.insert(edge.clone(), 1);
+                    edge_to_tile_count.insert(id, 1);
                 }
                 Some(count) => {
-                    edge_to_tile_count.insert(edge.clone(), count + 1);
+                    edge_to_tile_count.insert(id, count + 1);
                 }
             }
         });
     });
 
-    // let tile_to_unique_edge_count = HashMap::new();
+    let mut tile_to_unique_edge_count = HashMap::new();
     input.tiles.iter().for_each(|tile| {
         let unique_edges = tile.
             edges().
             iter().
-            map(|edge| edge_to_tile_count.get(edge).unwrap()).
+            map(|edge| {
+                edge_to_tile_count.get(&edge.identifier()).unwrap()
+            }).
             filter(|count| **count == 1).
             count();
-        // tile_to_unique_edge_count.insert(tile, unique_edges);
+        tile_to_unique_edge_count.insert(tile.identifier, unique_edges);
     });
 
 }
@@ -43,6 +47,7 @@ enum Pixel {
 }
 #[derive(Hash, Debug, Clone)]
 struct Tile {
+    identifier: usize,
     pixels: Vec<Vec<Pixel>>,
 }
 
@@ -107,9 +112,16 @@ impl Edge {
 fn parse_input() -> io::Result<InputData> {
     let content = fs::read_to_string("inputs/day20.txt")?;
     let tile_lines = content.split("\n\n");
+    let identifier_regex = Regex::new(r"^Tile (?P<digits>\d+):$").unwrap();
 
     let tiles = tile_lines.map(|lines| {
-        let pixels = lines.split("\n").skip(1).map(|line| {
+        let mut iter = lines.split("\n");
+        let identifier_line = iter.next().unwrap();
+        let identifier: usize = identifier_regex.
+            captures(identifier_line).
+            and_then(|cap| cap.name("digits").and_then(|m| m.as_str().parse().ok())).
+            unwrap();
+        let pixels = iter.map(|line| {
             line.chars().map(|c| {
                 match c {
                     '.' => Pixel::Off,
@@ -118,7 +130,10 @@ fn parse_input() -> io::Result<InputData> {
                 }
             }).collect()
         }).collect();
-        Tile { pixels: pixels }
+        Tile {
+            identifier: identifier,
+            pixels: pixels,
+        }
     }).collect();
 
     Ok(InputData {
