c651a318766219b0edb4b01688bf9aac24f465a7
Wenley Tong <wenley.tong@gmail.com>
2020-12-20 21:04:03 -0800

    Cleanup template

--- src/template.rs
+++ src/template.rs
@@ -1,9 +1,6 @@
 extern crate regex;
 
-use std::io;
 use std::fs;
-use std::io::BufRead;
-use std::path::Path;
 
 fn main() {
     let input = parse_input();
@@ -17,25 +14,9 @@ impl InputData {
 }
 
 fn parse_input() -> InputData {
-    let io_result = lines_in_file("day3input.txt");
-    match io_result {
-        Ok(lines) => {
-            let rows = lines.map(|line| match line {
-                Ok(stuff) => {
-                    stuff
-                }
-                Err(_) => panic!("Error reading line"),
-            }).collect();
-            InputData {
-                rows: rows,
-            }
-        },
-        Err(_) => panic!("Error reading file"),
+    let content = fs::read_to_string("inputs/day20.txt").unwrap();
+    InputData {
+        rows: content.split("\n").map(|s| s.to_string()).collect(),
     }
-}
-
-fn lines_in_file<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<fs::File>>> where P: AsRef<Path> {
-    let file = fs::File::open(file_path)?;
-    Ok(io::BufReader::new(file).lines())
 }
 
