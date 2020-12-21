ee155cc876fdababaaf009d97bac84e11b46d7a5
Wenley Tong <wenley.tong@gmail.com>
2020-12-20 18:37:56 -0800

    Cleanup Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,63 +1,61 @@
 extern crate regex;
 
-use std::io;
 use std::fs;
-use std::io::BufRead;
-use std::path::Path;
 use regex::Regex;
 
 fn main() {
-    let io_result = lines_in_file("input/day2.txt");
-    match io_result {
-        Ok(lines) => {
-            let mut valid_rows = 0;
-            for line in lines {
-                match line {
-                    Ok(stuff) => {
-                        if parse_line(&stuff).valid() {
-                            valid_rows += 1;
-                        }
-                    }
-                    Err(_) => panic!("Error reading line"),
-                }
-            }
-            println!("{} rows passed rules", valid_rows);
-        },
-        Err(_) => panic!("Error reading file"),
-    }
+    let input_rules = parse_input();
+    let valid_rows = input_rules.iter().filter(|rule| rule.valid()).count();
+    let valid_rows_2 = input_rules.iter().filter(|rule| rule.valid_2()).count();
+    println!("{} rows passed Part 1 rules", valid_rows);
+    println!("{} rows passed Part 2 rules", valid_rows_2);
 }
 
-fn lines_in_file<P>(file_path: P) -> io::Result<io::Lines<io::BufReader<fs::File>>> where P: AsRef<Path> {
-    let file = fs::File::open(file_path)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-struct PasswordRule<'a> {
-    min_count: i64,
-    max_count: i64,
+struct PasswordRule {
+    x: usize,
+    y: usize,
     letter: char,
-    test_password: &'a str,
+    test_password: String,
 }
 
-fn parse_line(line: &str) -> PasswordRule {
+fn parse_input() -> Vec<PasswordRule> {
+    let content = fs::read_to_string("inputs/day2.txt").unwrap();
     let parse_regex: Regex = Regex::new(r"^(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]): (?P<pass>[a-z]+)$").unwrap();
-    let captures: regex::Captures = match parse_regex.captures(line) {
-        Some(thing) => thing,
-        None => panic!("Couldn't parse {}", line),
-    };
-
-    PasswordRule {
-        min_count: captures.name("min").unwrap().as_str().parse().unwrap(),
-        max_count: captures.name("max").unwrap().as_str().parse().unwrap(),
-        letter: captures.name("letter").unwrap().as_str().chars().next().unwrap(),
-        test_password: captures.name("pass").unwrap().as_str(),
-    }
+    content.split("\n").map(|line| {
+        parse_regex.captures(line).map(|cap| {
+            PasswordRule {
+                x: cap.name("min").and_then(|m| m.as_str().parse().ok()).unwrap(),
+                y: cap.name("max").and_then(|m| m.as_str().parse().ok()).unwrap(),
+                letter: cap.name("letter").and_then(|m| m.as_str().chars().next()).unwrap(),
+                test_password: cap.name("pass").unwrap().as_str().to_string(),
+            }
+        }).unwrap()
+    }).collect()
 }
 
-impl PasswordRule<'_> {
+impl PasswordRule {
     fn valid(&self) -> bool {
-        let letter_counts = self.test_password.chars().filter(|c| *c == self.letter).count() as i64;
+        let letter_counts = self.test_password.chars().filter(|c| *c == self.letter).count();
 
-        letter_counts >= self.min_count && letter_counts <= self.max_count
+        letter_counts >= self.x && letter_counts <= self.y
+    }
+
+    fn valid_2(&self) -> bool {
+        let position1_matches = match self.test_password.chars().nth(self.x - 1) {
+            Some(c) => c == self.letter,
+            None => {
+                eprintln!("index {} is longer than {} ({} long)", self.x, self.test_password, self.test_password.len());
+                false
+            }
+        };
+        let position2_matches = match self.test_password.chars().nth(self.y - 1) {
+            Some(c) => c == self.letter,
+            None => {
+                eprintln!("index {} is longer than {} ({} long)", self.y, self.test_password, self.test_password.len());
+                false
+            }
+        };
+
+        position1_matches ^ position2_matches
     }
 }
